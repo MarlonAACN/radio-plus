8dd08d0f3139397d66fe9aa105b2c70d4043bcd8
Marlon Kerth <MarlonAACN@users.noreply.github.com>
2024-03-11 10:43:39 +0100

    Bump cypress timeout for webserver startup

--- apps/frontend-e2e/cypress.config.ts
+++ apps/frontend-e2e/cypress.config.ts
@@ -3,6 +3,7 @@ import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
+  pageLoadTimeout: 30000,
   e2e: {
     ...nxE2EPreset(__filename, {
       cypressDir: 'src',

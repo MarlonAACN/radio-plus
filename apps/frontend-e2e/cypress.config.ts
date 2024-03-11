4c5eb26d26948a006f9305173872ca583374af66
Marlon Kerth <MarlonAACN@users.noreply.github.com>
2024-03-11 11:44:41 +0100

    Add timeout for frontend e2e

--- apps/frontend-e2e/cypress.config.ts
+++ apps/frontend-e2e/cypress.config.ts
@@ -3,12 +3,14 @@ import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
-  pageLoadTimeout: 30000,
   e2e: {
     ...nxE2EPreset(__filename, {
       cypressDir: 'src',
       webServerCommands: { default: 'nx run frontend:start' },
       ciWebServerCommand: 'nx run frontend:serve-static',
+      webServerConfig: {
+        timeout: 30000,
+      }
     }),
     baseUrl: 'http://localhost:3000',
   },

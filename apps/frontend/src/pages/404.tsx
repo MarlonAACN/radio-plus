6ccc185fef96a3cfe05a6c436aa9adf3aad1ec33
Marlon Kerth <MarlonAACN@users.noreply.github.com>
2024-06-16 16:43:03 +0200

    next static pages error

--- apps/frontend/src/pages/404.tsx
+++ apps/frontend/src/pages/404.tsx
@@ -1,21 +1,8 @@
-import Link from 'next/link';
-
-import { appRouter } from '@/router/app/AppRouter';
-
 function NotFoundPage() {
   return (
-    <>
-      <main>
-        <h1>404 - Page not found</h1>
-        <Link
-          href={appRouter.get('Base').build()}
-          className="px-2 py-1 mx-1 text-white bg-primary-500 rounded"
-          aria-label="Go back to the landing page"
-        >
-          Home
-        </Link>
-      </main>
-    </>
+    <main>
+      <h1>404 - Page not found</h1>
+    </main>
   );
 }
 
